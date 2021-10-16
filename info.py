9c91de71cc7b3d3f9f62616586ec701f07560838
kirito <manjrekarom@gmail.com>
2021-10-16 03:23:41 -0400

    Add requirements, crypto and common info

---
+++ info.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ValidatorInfo:
+    author: str
+    public_key: str
+    private_key: str
+    client_public_key: str
+    f: int  # f is upper bound on number of byzantine faults
