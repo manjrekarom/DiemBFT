1a91829de2d9205cb223712e465e70543a921797
kirito <manjrekarom@gmail.com>
2021-10-16 21:43:25 -0400

    Multiple item hashing

--- src/crypto.py
+++ src/crypto.py
@@ -3,8 +3,21 @@ from nacl.hash import sha256
 
 _encoder = nacl.encoding.Base64Encoder
 
-def hasher(msg):
-    return sha256(msg, encoder=_encoder)
+def hasher(*items) -> str:
+    """
+    Return digest as string
+    """
+    msg = ''
+    items = list(map(repr, items))
+    msg = '|||'.join(items)
+    if msg is str:
+        msg = msg.encode('utf-8')
+    else:
+        msg = repr(msg).encode('utf8')
+    return sha256(msg, encoder=_encoder).decode('utf-8')
 
 def sign(msg, private_key):
+    raise NotImplementedError
+
+def valid_signatures(block: 'Block', last_tc: 'TC'):
     raise NotImplementedError
